7f406da0ebcea0e017bbf856ae396e01de72f35a
Kareem-Ismail1 <kisma@uic.edu>
2026-04-26 21:44:43 -0500

    Add files via upload

---
+++ Main(1).cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+/*
+Kareem Ismail
+Date: April 26, 2026
+Program Description: 
+The ultimate way to both practice technical interview questions(LeetCode type questions) and 
+have fun. The purpose of this is exactly that, provide more experience with LeetCode type 
+questions in a 1v1, 2v2, or 4v4 format. The team that completes the question within the 
+time limit first wins, if no one solves it, the winner is the one who got more test cases 
+passed. This solves the problem of wanting to practice but not having any incentive, 
+since it is fun, it will drive you to play, also the rank system also has incentive
+since people generally want to be ranked high. Compete against friends, or aim to 
+be the best while learning!
+*/
+
+int main() 
+{
+    char iOne;
+    
+    char iTwo;
+    
+    string outputOne;
+    
+    string outputTwo;
+    
+    cout << "Welcome to CodeRacer!" << endl;
+    
+    cout << endl; 
+    
+    cout << "Player 1: Please press a key";
+    
+    cin >> iOne;
+    
+    cout << "Player 2: Please press a key";
+    
+    cin >> iTwo;
+    
+    cout << endl;
+    
+    cout << "On Go! the prompt will be shown and players will start coding" << endl;
+    
+    cout << "Ready!" << endl;
+    
+    cout << "Set." << endl;
+    
+    cout << "Go!" << endl;
+    
+    cout << endl;
+    
+    cout << "Prompt no white space:" << endl << endl << "Write a line of code that correctly outputs Hello World!" << endl;
+    
+    cin >> outputOne;
+    
+    cin >> outputTwo;
+    
+    cout << "Player 1 Wins!" << endl; 
+  
+    return 0;
+}
